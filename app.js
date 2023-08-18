3b83f2326052cf48ddadd6cb003f5507d9d59469
Ashwin <ashwinkv.akv@gmail.com>
2023-08-18 11:17:55 +0530

    fianl update

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const session = require('express-session');
 const app = express();
 const port = 3000;
 
+const data = [{img:"https://www.havahart.com/media/wysiwyg/hh/cms/lc/mice/hh-animals-mouse-1.png",title:'mouse',des:'this mouse is very usefull you can use this to hover through the browser'},
+    {img:"https://www.havahart.com/media/wysiwyg/hh/cms/lc/mice/hh-animals-mouse-1.png",title:'mouse',des:'this mouse is very usefull you can use this to hover through the browser'},
+    {img:"https://www.havahart.com/media/wysiwyg/hh/cms/lc/mice/hh-animals-mouse-1.png",title:'mouse',des:'this mouse is very usefull you can use this to hover through the browser'},
+    {img:"https://www.havahart.com/media/wysiwyg/hh/cms/lc/mice/hh-animals-mouse-1.png",title:'mouse',des:'this mouse is very usefull you can use this to hover through the browser'},
+    {img:"https://www.havahart.com/media/wysiwyg/hh/cms/lc/mice/hh-animals-mouse-1.png",title:'mouse',des:'this mouse is very usefull you can use this to hover through the browser'},
+    {img:"https://www.havahart.com/media/wysiwyg/hh/cms/lc/mice/hh-animals-mouse-1.png",title:'mouse',des:'this mouse is very usefull you can use this to hover through the browser'},
+    {img:"https://www.havahart.com/media/wysiwyg/hh/cms/lc/mice/hh-animals-mouse-1.png",title:'mouse',des:'this mouse is very usefull you can use this to hover through the browser'}];
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +21,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: {
-        maxAge: 60 * 60 * 1000 // Set the cookie to expire in 30 days
+        maxAge: 60 * 60 * 1000 // Set the cookie to expire in 1 hour
     }
 }));
 
@@ -34,28 +41,41 @@ function authenticate(req, res, next) {
     }
 }
 
+//Disable caching
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-store');
+    next();
+});
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
     
-    // Simulated login logic (for demo purposes)
     const user = users.find(user => user.username === username && user.password === password);
     if (user) {
         req.session.user = user;
+        console.log(req);
         console.log(username+" logged in");
         res.redirect('/home');
     } else {
+        res.redirect('/wrong')
         // Pass an error message to the login view
-        res.render('login', { errorMessage: 'Incorrect username or password' });
     }
 });
+app.get('/wrong',(req,res)=>{
+    if (req.session.user) {
+        res.redirect('/home'); 
+    } else {
+        res.render('login', { errorMessage: 'Incorrect username or password' });
+    }
 
+})
 app.get('/',authenticate,(req, res) => {
-    res.render('home');
+    
+    res.render('home',{ data });
 });
 
 app.get('/home', authenticate,(req, res) => {
-        res.render('home');
+        res.render('home',{ data });
 });
 
 app.get('/logout',authenticate, (req, res) => {
@@ -64,7 +84,7 @@ app.get('/logout',authenticate, (req, res) => {
     res.redirect('/');
 });
 app.get('*', (req, res) => {
-    res.status(404).send('404'); // Render a custom 404 page
+    res.status(404).send('404');
 });
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
